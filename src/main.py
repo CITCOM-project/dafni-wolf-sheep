0e194b6c186d3f7fc5fb5e9d81714b3603c05eb3
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-06-22 12:11:46 +0100

    csv output

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from wolf_sheep import model
 
-wsmodel = model.WolfSheep(initial_sheep=100, initial_wolves=50)
+wsmodel = model.WolfSheep(initial_sheep = 100, initial_wolves = 50, verbose = False)
 
 wsmodel.run_model()
+
+wsmodel_df = wsmodel.datacollector.get_model_vars_dataframe()
+
+wsmodel_df.to_csv('wolf_sheep_out.csv')
